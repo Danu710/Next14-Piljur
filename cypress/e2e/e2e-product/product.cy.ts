a5a1648743dfe30071284bd20e6e370da126fd8c
Ramadhanu Kadir Rachono <90473066+Danu710@users.noreply.github.com>
2024-02-27 11:39:11 +0700

    update response data api blue-bird

--- cypress/e2e/e2e-product/product.cy.ts
+++ cypress/e2e/e2e-product/product.cy.ts
@@ -2,18 +2,17 @@ describe('pages product test', () => {
   it('should data product', () => {
     cy.visit('/product');
 
-    // Check if cards are displayed
     cy.get('.grid').children().should('have.length.at.least', 1);
-
-    // Check if each card contains necessary elements
-    cy.get('.grid')
-      .children()
-      .each((card) => {
-        cy.wrap(card).within(() => {
-          cy.get('div').should('exist');
-          cy.get('img').should('exist');
-          cy.get('p').should('exist');
-        });
-      });
+  });
+  it('Should successfully fetch data from the API', () => {
+    cy.request(
+      'GET',
+      'https://private-f2fbfb-ridecar2.apiary-mock.com/vehicles'
+    ).then((response) => {
+      expect(response.status).to.eq(200); // Ensure the response status is 200 OK
+      expect(response.body).to.have.property('category').to.be.an('array');
+      expect(response.body).to.have.property('type').to.be.an('array');
+      // Add more assertions as needed based on the structure of your API response
+    });
   });
 });
